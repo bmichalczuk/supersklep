d9144e8f7efc1184435c43ed7fe6a26cbe10a5f1
bmichalczuk <barmic90@hotmail.com>
2023-10-08 22:53:13 +0200

    change generateStaticParams param

--- src/app/products/[pageNumber]/page.tsx
+++ src/app/products/[pageNumber]/page.tsx
@@ -1,17 +1,13 @@
 import { getProductsList } from "../../api/products";
 import { ProductsList } from "@/ui/organisms/ProductsList";
 
-export const generateStaticParams = async ({
-	pageNumber,
-}: {
-	pageNumber: string;
-}) => {
+export const generateStaticParams = async () => {
 	const numberOfProducts = (await getProductsList(100)).length;
 	const numberOfPages = Math.ceil(numberOfProducts / 20);
 
 	const params = [];
 	for (let i = 1; i <= numberOfPages; i++) {
-		params.push(i);
+		params.push(String(i));
 	}
 	return params;
 };
