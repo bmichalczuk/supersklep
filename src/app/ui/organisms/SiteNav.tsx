079d83de91a867785bc28a2ea17976c95d61e37d
bmichalczuk <barmic90@hotmail.com>
2023-10-14 23:40:48 +0200

    da

--- src/app/ui/organisms/SiteNav.tsx
+++ src/app/ui/organisms/SiteNav.tsx
@@ -5,17 +5,17 @@ export const SiteNav = () => {
 		"p-5 font-medium uppercase tracking-wider";
 	const activeLinkActiveClassName = "bg-teal-400";
 	return (
-		<nav className="col-span-4 flex h-full items-center  justify-center">
-			<ul
-				className="flex justify-center space-x-1  "
-				role="navigation"
-			>
+		<nav
+			className="col-span-4 flex h-full items-center  justify-center"
+			role="navigation"
+		>
+			<ul className="flex justify-center space-x-1  ">
 				<li>
 					<ActiveLink
 						className={activeLinkClassName}
 						activeClassName={activeLinkActiveClassName}
 						href="/"
-						title="Strona Główna"
+						title="Home"
 					>
 						Home
 					</ActiveLink>
@@ -23,7 +23,7 @@ export const SiteNav = () => {
 				<li>
 					<ActiveLink
 						href="/products"
-						title="Lista produktów"
+						title="All"
 						className={activeLinkClassName}
 						activeClassName={activeLinkActiveClassName}
 						exact={false}
